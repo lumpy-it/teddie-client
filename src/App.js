3faed9d0e085dca196bbfc63a8dacba7722f4ce1
Kalu <info@zap.de.com>
2017-05-24 07:23:36 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+import 'teddie-bootstrap';
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface
+} from 'react-apollo';
+
+import DoctrineList from './components/DoctrineList';
+import DoctrineAdd from './components/DoctrineAdd';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const client = new ApolloClient({
+  networkInterface
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div>
+          <Navbar fixedTop>
+              <Navbar.Header>
+                <Navbar.Brand>
+                  <a href="/">TEDDIE</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+              </Navbar.Header>
+          </Navbar>
+          <Grid style={{paddingTop: '60px'}}>
+            <Jumbotron>
+                <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <p><Button bsStyle="primary">Learn more</Button></p>
+            </Jumbotron>
+            <DoctrineList/>
+            <DoctrineAdd/>
+          </Grid>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/DoctrineList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {
+  gql,
+  graphql
+} from 'react-apollo';
+
+import '../schema/schema.js';
+
+const doctrineListQuery = gql`
+  query DoctrineListQuery {
+    doctrines {
+      id
+      name
+      tags
+    }
+  }
+`;
+
+const DoctrineList = ({data: {loading, error, doctrines}}) => {
+  if (loading) {
+    return <p>loading...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>
+  }
+  return (
+    <div>
+        <ul>
+            {doctrines.map(d => <li key={d.id}>{d.name} ({d.tags.join(" ")})</li>)}
+        </ul>
+    </div>
+  );
+}
+
+export default graphql(doctrineListQuery)(DoctrineList);
